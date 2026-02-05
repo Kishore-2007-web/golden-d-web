e1a7c949640ff09cd0668193637fb41a6cc68e59
Hariprasath2611 <hp5173690@gmail.com>
2026-02-06 02:48:07 +0530

    Updated project files

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "0.0.0.0",
-    port: 8081,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
